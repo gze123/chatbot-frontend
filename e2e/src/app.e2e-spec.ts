5e7b300a70a58a2720a9a636546f1ce430aa105d
Ziy En <gze.2315@gmail.com>
2020-06-15 01:35:08 +0800

    try deploy

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('FinalYearProject-Frontend app is running!');
+    expect(page.getTitleText()).toEqual('chatbot-frontend app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'FinalYearProject-Frontend'`, () => {
+  it(`should have as title 'chatbot-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('FinalYearProject-Frontend');
+    expect(app.title).toEqual('chatbot-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('FinalYearProject-Frontend app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('chatbot-frontend app is running!');
   });
 });
